62b8afd38e42ceaa1dca49493f4cc15c82d3c6b2
Amy Frieson <ava447@gmail.com>
2020-04-04 15:18:07 -0400

    Add Info component

--- src/components/ListWrapper/index.js
+++ src/components/ListWrapper/index.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import './index.css';
 
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
 import List from '../List';
 import Toggle from '../Toggle';
 import config from '../../config';
@@ -16,32 +14,15 @@ export default function ListWrapper() {
     filterDisplayTitles,
     filterKeys,
     offerString,
-    requestString,
-    responseEmail
+    requestString
   } = config;
   const [type, setType] = useState(filterKeys.all);
 
   return (
-    <Container className="ListWrapper">
-      <div className="border-bottom mb-3">
-        <Button
-          className="mr-2 mb-3"
-          href={'/submit'}
-          variant="info"
-        >
-          Submit a need or offer
-        </Button>
-        <Button
-          className="mb-3"
-          href={`mailto:${responseEmail}`}
-          variant="info"
-        >
-          Email us to respond to a listing
-        </Button>
-      </div>
+    <div className="ListWrapper">
       <h2>Listings</h2>
       <section>
-        <h3 class="sr-only sr-only-focusable">Types</h3>
+        <h3 className="sr-only sr-only-focusable">Types</h3>
         <Toggle
           displayTitles={filterDisplayTitles}
           handleChange={setType}
@@ -58,13 +39,6 @@ export default function ListWrapper() {
         requestString={requestString}
         type={type}
       />
-      <Button
-        className="mb-3"
-        href={`mailto:${responseEmail}`}
-        variant="info"
-      >
-        Email us to respond to a listing
-      </Button>
-    </Container>
+    </div>
   );
 }
